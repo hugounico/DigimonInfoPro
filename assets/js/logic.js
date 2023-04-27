5d8cc1cd024fdde0d95ca7ffd2e232ea53d81a60
hugounico <hcontrerassantana@gmail.com>
2023-04-27 10:48:19 -0400

    okok

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,16 +6,17 @@ $(document).ready(function() {                                  //cambia de colo
     });
 });
 
-/*document.addEventListener('DOMContentLoaded', function() { */  //dispara la función luego de que el html se a caragdo correctamente
-    fetch("https://digimon-api.vercel.app/api/digimon")
-        .then(response => response.json())
-        .then(data => {                                   //data es donde se traera la informacion
+let data;
 
-            const tableBody = document.getElementById("table-body");
-            let count = 0;
+fetch("https://digimon-api.vercel.app/api/digimon")
+    .then(response => response.json())
+      .then(data => {                                   //data es donde se traera la informacion
+
+        const tableBody = document.getElementById("table-body");
+          let count = 0; //crea la variable count que parte en 0
         
             data.forEach(digimon => {
-              count++;
+              count++; //aumenta la variable count de 1 en 1 para cada iteracion
               const row = document.createElement("tr");
               const numberCell = document.createElement("td");
               const imgCell = document.createElement("td");
@@ -23,7 +24,8 @@ $(document).ready(function() {                                  //cambia de colo
               const levelCell = document.createElement("td");
         
               numberCell.textContent = count;
-              imgCell.innerHTML = `<img src="${digimon.img}" alt="${digimon.name}">`;
+              imgCell.innerHTML = `<img src="${digimon.img}" alt="${digimon.name}">`; /*PROBANDO DATA-ID para que al hacer click en al imagen abra un card*/
+              imgCell.id = "img"+count;
               nameCell.textContent = digimon.name;
               levelCell.textContent = digimon.level;
         
@@ -34,9 +36,26 @@ $(document).ready(function() {                                  //cambia de colo
         
               tableBody.appendChild(row);
             });
-        })
+      })
     .catch(error => console.error('Error:', error));
-/*})*/
+
+const pokemonImgs = document.querySelectorAll('#myTable');
+pokemonImgs.forEach(img => {
+  img.addEventListener('click', () => {
+    
+    //alert("Por favor, complete el campo Nombre.")
+    //console.log(img.getAttribute("id"));
+    
+    /*const pokemon = data[img.getAttribute('data-id')];
+    document.getElementById('modal-pokemon-name').textContent = pokemon.name;
+    document.getElementById('modal-pokemon-img').src = pokemon.img;
+    document.getElementById('modal-pokemon-level').textContent = pokemon.level;
+    const modal = new bootstrap.Modal(document.getElementById('modal'));
+    modal.show();*/
+
+  });
+});
+
 
 //document.getElementById('title').textContent = data.title; //lo verde es el id que esta en el html
 //document.getElementById('poster').src = data.poster;  //lo que esta despues del data. "poster" es el id a buscar en la api
